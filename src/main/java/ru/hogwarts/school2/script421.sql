b8dbd56c9ccae990226b1d3c167328e91d9a1d7e
MrFoxCool <112571101+MrFoxCool@users.noreply.github.com>
2024-11-18 22:48:57 +0500

    HW 4.2

--- src/main/java/ru/hogwarts/school2/script421.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE student ADD CONSTRAINT age_constraint(age>16);
-ALTER TABLE student ADD CONSTRAINT name UNIQUE(name);
-ALTER TABLE faculty ADD CONSTRAINT color_name_unique UNIQUE(name,color);
-ALTER TABLE student SET DEFAULT 20;
